639769f69273c9c59dd5ddc2388b18152f216373
Andrey Radionov <andyomsk@gmail.com>
2018-03-17 21:36:42 +0600

    TFCM.03-Ex Completed

---
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseInstanceIdService.java
@@ -0,0 +1,21 @@
+package android.example.com.squawker.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * @author Andrey Radionov
+ */
+
+public class SquawkFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    private static final String TAG = SquawkFirebaseInstanceIdService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+    }
+}
